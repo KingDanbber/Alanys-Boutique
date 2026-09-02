dc19c4f0d991b0d08d681a87cf3b29e635a33964
DanKing92 <147676501+KingDanbber@users.noreply.github.com>
2026-09-01 19:08:48 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ import { renderDashboard } from './modules/dashboard.js'
 import { renderProducts } from './modules/products.js'
 import { renderPlaceholder } from './modules/placeholder.js'
 import { renderClients } from './modules/clients.js'
+import { renderOrders } from './modules/orders.js'
 import { initPWA } from './lib/pwa.js'
 import {
   renderDevOverview,
@@ -23,7 +24,7 @@ const routes = {
   '/register': renderRegister,
   '/dashboard': renderDashboard,
   '/productos': renderProducts,
-  '/pedidos': (root) => renderPlaceholder(root, 'Pedidos', 'pedidos'),
+  '/pedidos': renderOrders,
   '/clientes': renderClients,
   '/gastos': (root) => renderPlaceholder(root, 'Gastos', 'mas'),
   '/mas': (root) => renderPlaceholder(root, 'Más', 'mas'),
